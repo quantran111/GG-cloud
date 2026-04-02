a2c2004f807c4ba2e6315c4a8161d699d1221eb7
quantran111 <quantran07082005@gmail.com>
2026-04-02 13:23:42 +0700

    initial commit

---
+++ BaiDoXe/Create Data.sql
@@ -0,0 +1,44 @@
+DROP TABLE IF EXISTS `server1`;
+CREATE TABLE `server1` (
+  `soghe` varchar(255) DEFAULT NULL,
+  `tenkhach` varchar(255) DEFAULT NULL,
+  `loaive` varchar(255) DEFAULT NULL,
+  `thanhtoan` varchar(255) DEFAULT NULL,
+  `giodat` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `server2`;
+CREATE TABLE `server2` (
+  `soghe` varchar(255) DEFAULT NULL,
+  `tenkhach` varchar(255) DEFAULT NULL,
+  `loaive` varchar(255) DEFAULT NULL,
+  `thanhtoan` varchar(255) DEFAULT NULL,
+  `giodat` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `server3`;
+CREATE TABLE `server3` (
+  `soghe` varchar(255) DEFAULT NULL,
+  `tenkhach` varchar(255) DEFAULT NULL,
+  `loaive` varchar(255) DEFAULT NULL,
+  `thanhtoan` varchar(255) DEFAULT NULL,
+  `giodat` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `server4`;
+CREATE TABLE `server4` (
+  `soghe` varchar(255) DEFAULT NULL,
+  `tenkhach` varchar(255) DEFAULT NULL,
+  `loaive` varchar(255) DEFAULT NULL,
+  `thanhtoan` varchar(255) DEFAULT NULL,
+  `giodat` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `server5`;
+CREATE TABLE `server5` (
+  `soghe` varchar(255) DEFAULT NULL,
+  `tenkhach` varchar(255) DEFAULT NULL,
+  `loaive` varchar(255) DEFAULT NULL,
+  `thanhtoan` varchar(255) DEFAULT NULL,
+  `giodat` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
